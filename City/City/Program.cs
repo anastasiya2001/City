a6e24680305246326266848865bc3ba04bd8424a
Anastasia Shulikina <nastya.shulikina@yandex.ru>
2022-04-20 15:57:10 +0400

    change

--- City/City/Program.cs
+++ City/City/Program.cs
@@ -8,8 +8,10 @@ static void AdoNET()
     var connection = new SqlConnection("Server=MSI; Database=City2; Trusted_Connection=True;");
     connection.Open();
 
+
+    Console.WriteLine($"\n1. Список домов, отсортированных по давности ремонта:");
     var command = connection.CreateCommand();
-    command.CommandText = "SELECT a.number, b.name, a.amount, a.year_repair FROM homes a JOIN streets b ON a.id_streetID = b.ID";
+    command.CommandText = "SELECT a.number, b.name FROM homes a JOIN streets b ON a.id_streetID = b.ID order by(a.year_repair)";
 
     var reader = command.ExecuteReader();
     while (reader.Read())
@@ -17,18 +19,8 @@ static void AdoNET()
         Console.WriteLine($"Номер дома:{reader.GetString(0)} название:{reader.GetString(reader.GetOrdinal("name"))}");
     }
     reader.Close();
-    Console.WriteLine($"Список домов, отсортированных по давности ремонта:");
-    command = connection.CreateCommand();
-    command.CommandText = "SELECT a.number, b.name FROM homes a JOIN streets b ON a.id_streetID = b.ID order by(a.year_repair)";
 
-    reader = command.ExecuteReader();
-    while (reader.Read())
-    {
-        Console.WriteLine($"Номер дома:{reader.GetString(0)} название:{reader.GetString(reader.GetOrdinal("name"))}");
-    }
-    reader.Close();
-
-    Console.WriteLine($"Список домов, отсортированных по степени необходимости в ремонте:");
+    Console.WriteLine($"\n2. Список домов, отсортированных по степени необходимости в ремонте:");
     command = connection.CreateCommand();
     command.CommandText = "SELECT a.number, b.name FROM homes a JOIN streets b ON a.id_streetID = b.ID order by (select(a.amount * (2022 - a.year_repair)))desc";
 
@@ -39,7 +31,7 @@ static void AdoNET()
     }
     reader.Close();
 
-    Console.WriteLine($"Список домов, ремонтированных позже среднего по базе:");
+    Console.WriteLine($"\n3. Список домов, ремонтированных позже среднего по базе:");
     command = connection.CreateCommand();
     command.CommandText = "SELECT a.number, b.name FROM homes a, streets b where a.id_streetID = b.ID and a.year_repair < (select avg(year_repair) from homes)";
 
@@ -49,34 +41,144 @@ static void AdoNET()
         Console.WriteLine($"Номер дома:{reader.GetString(0)} название:{reader.GetString(reader.GetOrdinal("name"))}");
     }
     reader.Close();
-    Console.WriteLine($"Список домов, ремонтированных позже среднего по базе:");
+
+    Console.WriteLine($"\n4. Дома ремонтированные позднее 5-ти лет:");
     command = connection.CreateCommand();
-    command.CommandText = "SELECT a.number, b.name FROM homes a, streets b where a.id_streetID = b.ID and a.year_repair < (select avg(year_repair) from homes)";
+    command.CommandText = "SELECT b.name, count(a.number) as home_count FROM homes a, streets b where a.id_streetID = b.ID group by b.name order by count(a.number)";
 
     reader = command.ExecuteReader();
     while (reader.Read())
     {
-        Console.WriteLine($"Номер дома:{reader.GetString(0)} название:{reader.GetString(reader.GetOrdinal("name"))}");
+        Console.WriteLine($"Название улицы: {reader.GetString(0)} кличество домов: {reader.GetInt32(reader.GetOrdinal("home_count"))}");
     }
+    reader.Close();
+
+    Console.WriteLine($"\n5. Самый старый по ремонту дом и произвести в нем ремонт:");
+    Console.WriteLine($"До выполнения ремонта:");
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.number, b.name, a.year_repair FROM homes a, streets b where a.id_streetID = b.ID";
+
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Номер дома: {reader.GetString(0)} название улицы:{reader.GetString(reader.GetOrdinal("name"))} год ремонта {reader.GetInt32(reader.GetOrdinal("year_repair"))}");
+    }
+    reader.Close();
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.ID FROM homes a, streets b where a.year_repair = (select min(year_repair) from homes)";
+    reader = command.ExecuteReader();
+    //while (reader.Read())
+    //{
+    //    var connection_del = new SqlConnection(@"Server = MSI; Database = City; Trusted_Connection = True;");
+    //    connection_del.Open();
+    //    var command_del = connection_del.CreateCommand();
+    //   command_del.CommandText = $"UPDATE homes SET year_repair = 2022 WHERE id ={reader.GetInt32(reader.GetOrdinal("ID"))};";
+    //    command_del.ExecuteReader();
+    //    connection_del.Close();
+    //}
+    reader.Close();
+    Console.WriteLine($"\nПосле выполнения ремонта:");
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.number, b.name, a.year_repair FROM homes a, streets b where a.id_streetID = b.ID";
+
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Номер дома: {reader.GetString(0)} название улицы:{reader.GetInt32(reader.GetOrdinal("year_repair"))}");
+    }
+    reader.Close();
+
+ 
+
+    Console.WriteLine($"\n6. Самый маленький по числу квартир дом удалить:");
+    Console.WriteLine($"До удаления дома:");
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.number, b.name, a.amount FROM homes a, streets b where a.id_streetID = b.ID";
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Номер дома: {reader.GetString(0)} название улицы:{reader.GetString(reader.GetOrdinal("name"))} год ремонта {reader.GetInt32(reader.GetOrdinal("amount"))}");
+    }
+    reader.Close();
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.ID FROM homes a, streets b where a.amount = (select min(amount) from homes)";
+    reader = command.ExecuteReader();
+    //while (reader.Read())
+    //{
+    //    var connection_del = new SqlConnection(@"Server = MSI; Database = City; Trusted_Connection = True;");
+    //    connection_del.Open();
+    //    var command_del = connection_del.CreateCommand();
+    //    command_del.CommandText = $"DELETE homes WHERE ID = {reader.GetInt32(reader.GetOrdinal("ID"))};";
+    //    command_del.ExecuteReader();
+    //    connection_del.Close();
+    //}
+    reader.Close();
+    Console.WriteLine($"\nПосле удаления дома:");
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.number, b.name, a.amount FROM homes a, streets b where a.id_streetID = b.ID";
+
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Номер дома: {reader.GetString(0)} название улицы:{reader.GetInt32(reader.GetOrdinal("amount"))}");
+    }
+    reader.Close();
+
+
+    Console.WriteLine($"\n7. Поставить дом на наименее заселенной улице:");
+    Console.WriteLine($"До постройки дома, самая наименее заселённая:");
+    command = connection.CreateCommand();
+    command.CommandText = "select top(1) a.name, SUM(b.amount) as sum from streets a, homes b where a.ID = b.id_streetID GROUP BY a.name order by sum(b.amount)";
+    reader = command.ExecuteReader();
+    //while (reader.Read())
+    //{
+    //    Console.WriteLine($"Название улицы: {reader.GetString(0)} количество квартир:{reader.GetInt32(reader.GetOrdinal("sum"))}");
+    //}
+    //reader.Close();
+    command = connection.CreateCommand();
+    command.CommandText = $"select id from streets where name = '{reader.GetString(reader.GetOrdinal("name"))}'";
+    //reader = command.ExecuteReader();
+    //while (reader.Read())
+    //{
+    //    var connection_del = new SqlConnection(@"Server = MSI; Database = City; Trusted_Connection = True;");
+    //    connection_del.Open();
+    //    var command_del = connection_del.CreateCommand();
+    //    command_del.CommandText = $"INSERT INTO homes VALUES(25, {reader.GetInt32(reader.GetOrdinal("ID"))}, 300, 2022);";
+    //    command_del.ExecuteReader();
+    //    connection_del.Close();
+    //}
+    //reader.Close();
+    Console.WriteLine($"\nПосле постройки дома:");
+    command = connection.CreateCommand();
+    command.CommandText = "SELECT a.number, b.name, a.amount FROM homes a, streets b where a.id_streetID = b.ID";
+
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Номер дома: {reader.GetString(0)} название улицы:{reader.GetString(reader.GetOrdinal("name"))}");
+    }
+    reader.Close();
+
+
     connection.Close();
 }
 
 static void Requests()
 {
     Database db = new Database();
-    Console.WriteLine($"Список домов, отсортированных по давности ремонта:");
+    Console.WriteLine($"1. Список домов, отсортированных по давности ремонта:");
     foreach (home e in db.homes.OrderBy(e => e.year_repair))
     {
         Console.WriteLine($"{e.number} {e.id_street.name}");
     }
 
-    Console.WriteLine($"Список домов, отсортированных по степени потребности в ремонте:");
+    Console.WriteLine($"2. Список домов, отсортированных по степени потребности в ремонте:");
     foreach (home e in db.homes.OrderByDescending(e => ((2022 - e.year_repair) * e.amount)))
     {
         Console.WriteLine($"{e.number} {e.id_street.name}");
     }
 
-    Console.WriteLine($"Список домов, ремонтированных позже среднего по базе:");
+    Console.WriteLine($"3. Список домов, ремонтированных позже среднего по базе:");
     decimal sum = 0;
     decimal ave = 0;
     foreach (var e in db.homes)
@@ -89,14 +191,14 @@ static void Requests()
         Console.WriteLine($"{e.number} {e.id_street.name} {e.year_repair}");
     }
 
-    Console.WriteLine($"Дома ремонтированные позднее 5-ти лет:");
+    Console.WriteLine($"4. Дома ремонтированные позднее 5-ти лет:");
     foreach (var g in db.homes.Where(e => e.year_repair < 2017).GroupBy(e => e.id_street.name)
             .Select(x => new { street_name = x.Key, count = x.Count() }))
     {
         Console.WriteLine($"{g.street_name} {g.count}");
     }
 
-    Console.WriteLine($"Самый старый по ремонту дом:");
+    Console.WriteLine($"5. Самый старый по ремонту дом:");
     decimal oldHome = 2030;
     foreach (home e in db.homes)
     {
@@ -116,7 +218,7 @@ static void Requests()
         Console.WriteLine($"{e.number} {e.id_street.name} {e.year_repair}");
     }
 
-    Console.WriteLine($"Самый маленький по числу квартир дом удалить \n До:");
+    Console.WriteLine($"6. Самый маленький по числу квартир дом удалить \n До:");
     foreach (home e in db.homes)
     {
         Console.WriteLine($"{e.number} {e.id_street.name}");
@@ -136,7 +238,7 @@ static void Requests()
         Console.WriteLine($"{e.number} {e.id_street.name}");
     }
 
-    Console.WriteLine($"Наименее заселенная улица:");
+    Console.WriteLine($"7. Наименее заселенная улица:");
     int zasel = db.homes.Min(e => Convert.ToInt32(e.id_street.name.Count()) * Convert.ToInt32(e.amount));
     {
         foreach (home e in db.homes)
